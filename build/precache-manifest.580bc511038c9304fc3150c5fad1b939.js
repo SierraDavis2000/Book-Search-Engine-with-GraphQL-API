3036a5fd27c29350d15d75db02fefb07760c0939
SierraDavis2000 <99284604+SierraDavis2000@users.noreply.github.com>
2022-07-17 20:49:18 -0400

    final commits2

---
+++ build/precache-manifest.580bc511038c9304fc3150c5fad1b939.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe18b960c98c3cf20aec40ab97a3a12b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1482f06ee8267d2c1c86",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a3edaffb47bf5a40c2fb",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "1482f06ee8267d2c1c86",
+    "url": "/static/js/2.65ecffdf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.65ecffdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3edaffb47bf5a40c2fb",
+    "url": "/static/js/main.85375b05.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
